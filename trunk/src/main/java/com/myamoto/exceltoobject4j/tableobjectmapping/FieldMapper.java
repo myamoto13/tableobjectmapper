e7a75dbb23b6407d3386710dca7e8113221d55a2
im.michael.cortes@gmail.com <im.michael.cortes@gmail.com@f230e840-8b33-2dff-a715-1764df3ab351>
2013-09-02 21:30:19 +0000

    Maven Jaxb plugin

--- trunk/src/main/java/com/myamoto/exceltoobject4j/tableobjectmapping/FieldMapper.java
+++ trunk/src/main/java/com/myamoto/exceltoobject4j/tableobjectmapping/FieldMapper.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.6 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2013.09.02 à 11:18:07 PM CEST 
+// Généré le : 2013.09.02 à 11:24:01 PM CEST 
 //
 
 
